9ded209b9e18a95241c306dd788dd38b392bafeb
patelprince0555 <patelprince0555@gmail.com>
2024-02-07 10:03:28 +0530

    07-02-2024

--- 02-Day-02-Operators-in-java/practice_qs.java
+++ 02-Day-02-Operators-in-java/practice_qs.java
@@ -34,7 +34,16 @@ public class practice_qs {
 
 
        // question 4
-       
+    //    int x=9;
+    //    int y = 12;
+    //    int a=2;
+    //    int b = 4;
+    //    int c=6;
+    //    int exp= 4/3*(x+34)+9*(a+b*c)+(3+y*(2+a))/(a+b*y);
+    //    System.out.println(exp);
+
+
+    
     }
     
 }
