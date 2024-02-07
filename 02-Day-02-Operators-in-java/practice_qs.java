c88dd3095e151d79ccbd8ae230785038d21543ea
patelprince0555 <patelprince0555@gmail.com>
2024-02-07 09:45:44 +0530

    07-02-2024

---
+++ 02-Day-02-Operators-in-java/practice_qs.java
@@ -0,0 +1,17 @@
+public class practice_qs {
+    public static void main(String[] args) {
+
+        // question 1
+        // int x=2;
+        // int y= 5;
+        // int exp1=(x*y/x);
+        // int exp2=(x*(y/x));
+        // System.out.println(exp1+",");
+        // System.out.println(exp2);
+
+
+        // question 2
+        
+    }
+    
+}
