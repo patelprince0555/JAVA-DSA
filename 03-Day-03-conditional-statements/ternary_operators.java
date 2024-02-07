c326f51889ac7cc96154e4f4a08f461aedde9aee
patelprince0555 <patelprince0555@gmail.com>
2024-02-07 12:49:33 +0530

    07-02-2024

---
+++ 03-Day-03-conditional-statements/ternary_operators.java
@@ -0,0 +1,7 @@
+public class ternary_operators {
+    public static void main(String[] args) {
+        int marks= 50;
+        String status = (marks>33)?"pass":"fail";
+        System.out.println(status);
+    }
+}
