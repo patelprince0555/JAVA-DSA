3df348257360c2b801e2e8939bd63524886e324c
patelprince0555 <patelprince0555@gmail.com>
2024-02-07 09:52:28 +0530

    07-02-2024

--- 02-Day-02-Operators-in-java/practice_qs.java
+++ 02-Day-02-Operators-in-java/practice_qs.java
@@ -11,7 +11,17 @@ public class practice_qs {
 
 
         // question 2
-        
+        int x=200 ,y=50 , z=100;
+        if(x>y&&y>z){
+            System.out.println("Hello");
+        }
+
+        if(z>y&&z<x){
+            System.out.println("Java");
+        }
+        if((y+200)>x&&(y+150)>z){
+            System.out.println("hello java");
+        }
     }
     
 }
