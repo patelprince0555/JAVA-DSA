c29a92e40b55ef857e83db39d8f724b36d2e1065
patelprince0555 <patelprince0555@gmail.com>
2024-02-07 19:22:05 +0530

    07-02-2024

---
+++ 04-Day-04-loops/print_number_from_1to10.java
@@ -0,0 +1,10 @@
+public class print_number_from_1to10 {
+    public static void main(String[] args) {
+        int counter = 1;
+        while(counter<=10){
+            System.out.println(counter);
+            counter++;
+        }
+        System.out.println("printed number from 1 to 10");
+    }
+}
---
+++ 04-Day-04-loops/while_loop.java
@@ -0,0 +1,10 @@
+public class while_loop{
+    public static void main(String[] args) {
+        int counter=0;
+        while(counter<=100){
+            System.out.println("Hello world");
+            counter++;
+        }
+        System.out.println("printed hello world 100 times");
+    }
+}
