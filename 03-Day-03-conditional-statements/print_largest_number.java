ed0120762d44c763046fcfeb8aa558e1746e3516
patelprince0555 <patelprince0555@gmail.com>
2024-02-07 11:39:24 +0530

    07-02-2023

---
+++ 03-Day-03-conditional-statements/print_largest_number.java
@@ -0,0 +1,12 @@
+public class print_largest_number{
+    public static void main(String[] args) {
+        int A=10;
+        int B=5;
+        if(A>=B){
+            System.out.println("A is largest of 2");
+        }
+        else{
+            System.out.println("B is largest of 2");
+        }
+    }
+}
