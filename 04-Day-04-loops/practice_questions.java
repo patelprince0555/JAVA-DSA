cf6319bf638e1a68a888e4300072dfd464be339e
patelprince0555 <patelprince0555@gmail.com>
2024-02-08 14:51:13 +0530

    08-02-2024

--- 04-Day-04-loops/practice_questions.java
+++ 04-Day-04-loops/practice_questions.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class practice_questions {
     public static void main(String[] args) {
 
@@ -6,5 +8,32 @@ public class practice_questions {
             System.out.println("Hello");
             i+=2;
         }
+
+
+        // question 2
+        Scanner sc = new Scanner(System.in);
+        int number;
+        int choice;
+        int evensum =0;
+        int oddsum =0;
+
+        do{
+            System.out.println("enter the number");
+            number= sc.nextInt();
+            if(number%2==0){
+                evensum = evensum+number;
+            }
+            else{
+                oddsum=oddsum+number;
+            }
+            System.out.println("do you want to continue ? press 1 for yes or 0 for no");
+            choice=sc.nextInt();
+        }
+       
+        while(choice==1);
+        System.out.println("sum of even numbers: "+evensum);
+        System.out.println("sum of odd numbers:"+oddsum);
+        
+
     }
 }
