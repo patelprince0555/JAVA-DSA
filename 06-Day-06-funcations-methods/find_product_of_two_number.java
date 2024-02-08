793ad89eef21cf4914bc90149db8645de3540410
patelprince0555 <patelprince0555@gmail.com>
2024-02-09 00:26:18 +0530

    09-02-2024

---
+++ 06-Day-06-funcations-methods/find_product_of_two_number.java
@@ -0,0 +1,13 @@
+public class find_product_of_two_number{
+
+    public static int productnumber(int a, int b){
+        int product = a*b;
+        return product;
+
+    }
+    public static void main(String[] args) {
+        int product= productnumber(3, 4);
+        System.out.println(product);
+        
+    }
+}
--- 06-Day-06-funcations-methods/funcations.java
+++ 06-Day-06-funcations-methods/funcations.java
@@ -6,7 +6,7 @@ public class funcations{
         System.out.println("hello world");
     }
 
-    public static int  calculatesum( int a, int b){
+    public static int  calculatesum( int a, int b){ // formal parameters
         int sum = a+b;
         return sum;
       
@@ -23,7 +23,7 @@ public class funcations{
         System.out.println("Enter a number");
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int sum = calculatesum(a,b);
+        int sum = calculatesum(a,b); // actual parameters
         System.out.println(sum);
        
       
