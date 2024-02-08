ebad7ee8a7d25d3a8c0dd5be3e04a18e32acb1ce
patelprince0555 <patelprince0555@gmail.com>
2024-02-08 12:59:37 +0530

    08-02-2024

---
+++ 04-Day-04-loops/practice_questions.java
@@ -0,0 +1,10 @@
+public class practice_questions {
+    public static void main(String[] args) {
+
+        // question 1
+        for(int i=1;i<5;i++){
+            System.out.println("Hello");
+            i+=2;
+        }
+    }
+}
