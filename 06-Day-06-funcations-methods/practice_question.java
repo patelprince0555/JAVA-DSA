bae8b25496f29a8abe2f41dd1d70e9d9b2f3e771
patelprince0555 <patelprince0555@gmail.com>
2024-02-12 20:24:32 +0530

    12-02-2023

---
+++ 06-Day-06-funcations-methods/practice_question.java
@@ -0,0 +1,11 @@
+public class practice_question {
+    public static int average(int a,int b,int c){
+        int average=(a+b+c)/3;
+        return average;
+    }
+    public static void main(String[] args) {
+        // question 1
+        System.out.println(average(100, 100, 100));
+        
+    }
+}
