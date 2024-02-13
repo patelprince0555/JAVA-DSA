abfd4b1cce3252d07f8850276571e4ef2757fb35
patelprince0555 <patelprince0555@gmail.com>
2024-02-13 11:50:44 +0530

    13-02-2024

--- 06-Day-06-funcations-methods/practice_question.java
+++ 06-Day-06-funcations-methods/practice_question.java
@@ -26,6 +26,17 @@ public class practice_question {
     }
 
 
+    public static int thesumofdigit(int n){
+        int sum=0;
+        while(n>0){
+            int lastdigit =n%10;
+            sum = sum+lastdigit;
+            n=n/10;
+        }
+        return sum;
+    }
+
+
     public static void main(String[] args) {
         // question 1
         System.out.println(average(100, 100, 100));
@@ -55,13 +66,17 @@ public class practice_question {
         }
 
 
-        // question
+        // question 4
      // math.min()   it is used to return the smallest of two values
     //math.max()    it returns the largest of two values
     // math.sqrt()  it is used to return the square root of a number
     //math.pow()    it returns the value of first argument raised to the power to second argument
     //math.avg()    This function is used to get the average value from the given expression.
     // math.abs()   it will return the absolute value of the given value
+
+
+    //  question 5
+    System.out.println(thesumofdigit(121));
 
     }
 }
