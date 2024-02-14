f2b523944238743c9f5dc9fc668fb1dd16fb6495
patelprince0555 <patelprince0555@gmail.com>
2024-02-14 18:39:43 +0530

    14-02-2024

---
+++ 08-Day-08-Array/linear_search_part_ii.java
@@ -0,0 +1,16 @@
+public class linear_search_part_ii {
+
+    public static int linearsearch(String fruits[],String key){
+        for(int i =0; i<fruits.length;i++){
+            if(fruits[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        String fruits[]={"apple","mango","banana","pineapple","orange"};
+        String key="orange";
+        System.out.println(linearsearch(fruits, key));
+    }
+}
