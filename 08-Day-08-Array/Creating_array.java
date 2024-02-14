900f267ea1f54653f5b69e2db69add92ea90e8a9
patelprince0555 <patelprince0555@gmail.com>
2024-02-14 14:52:17 +0530

    14-02-2024

--- 08-Day-08-Array/Creating_array.java
+++ 08-Day-08-Array/Creating_array.java
@@ -1,5 +1,10 @@
 public class Creating_array{
     public static void main(String[] args) {
-        
+        int marks[]=new int[50];
+        System.out.println(marks.length);
+
+        int numbers[]={1,2,3,5};
+        int morenumbers[]={3,7,2,7,8,9};
+        String fruits[]={"apple","mango","orange"};
     }
 }
