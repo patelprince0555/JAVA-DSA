ba6ed2cdbdc85dc9a354ae7bf0c175b63864e310
patelprince0555 <patelprince0555@gmail.com>
2024-02-15 17:55:58 +0530

    15-02-2024

--- 08-Day-08-Array/Reverse_an_array.java
+++ 08-Day-08-Array/Reverse_an_array.java
@@ -1,8 +1,21 @@
 public class Reverse_an_array {
     public static void reversearray(int numbers[]){
-        
+        int first =0;
+        int last = numbers.length-1;
+        while(first<last){
+            int temp = numbers[last];
+            numbers[last]=numbers[first];
+            numbers[first]=temp;
+            first++;
+            last--;
+        }
     }
     public static void main(String[] args) {
         int numbers[]={1,4,5,7,9,10};
+        reversearray(numbers);
+        for(int i= 0;i<numbers.length;i++){
+            System.out.print(numbers[i]+" ");
+        }
+        System.out.println();
     }
 }
