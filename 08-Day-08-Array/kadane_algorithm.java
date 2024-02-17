707de3b44febea6be9987171fc124c1f14655263
patelprince0555 <patelprince0555@gmail.com>
2024-02-17 12:47:50 +0530

    17-02-2024

---
+++ 08-Day-08-Array/kadane_algorithm.java
@@ -0,0 +1,20 @@
+public class kadane_algorithm {
+    public static void kadanes(int numbers[]){
+        int cs=0;
+        int ms=Integer.MIN_VALUE;
+        for(int i=0;i<numbers.length;i++){
+            cs=cs+numbers[i];
+            if(cs<0){
+                cs=0;
+            }
+            ms=Math.max(ms, cs);
+
+        }
+        System.out.println("The max subarray sum ="+ms);
+    }
+    public static void main(String[] args) {
+        int numbers[]={-2,-3,4,-1,-2,1,5,-3};
+        kadanes(numbers);
+        
+    }
+}
