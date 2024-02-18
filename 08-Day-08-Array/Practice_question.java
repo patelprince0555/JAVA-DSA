3e6896b20135f1c8462dca5be80d2a15f0e8e845
patelprince0555 <patelprince0555@gmail.com>
2024-02-18 19:50:14 +0530

    18-02-2024

--- 08-Day-08-Array/Practice_question.java
+++ 08-Day-08-Array/Practice_question.java
@@ -1,4 +1,5 @@
 public class Practice_question {
+    //  question 1
     public static boolean checknumber(int numbers[]){
         for(int i=0;i<numbers.length;i++){
             for(int j=i+1;j<numbers.length;j++){
@@ -13,8 +14,30 @@ public class Practice_question {
 
 
 
+
+
+
+    //  question 3
+    public static int buyandsellstocks(int numbers[]){
+        int buyprice=Integer.MAX_VALUE;
+        int maxprofit=0;
+        for(int i=0;i<numbers.length;i++){
+            if(buyprice<numbers[i]){
+                int profit =numbers[i]-buyprice;
+                maxprofit=Math.max(maxprofit, profit);
+            }
+            else{
+                buyprice=numbers[i];
+            }
+            
+        }
+        return maxprofit;
+    }
+
+
+
     public static void rotation(int numbers[]){
-        
+
     }
     public static void main(String[] args) {
         //  question 1
@@ -22,8 +45,21 @@ public class Practice_question {
         System.out.println(checknumber(nums));
 
 
+
+
+
         //  question 2
         int nums1[]={4,5,6,7,0,1,2};
+
+
+
+
+        //  question 3
+        int prices[] ={7,1,5,3,6,4};
+        System.out.println(buyandsellstocks(prices));
+
+        int prices2[]={7,6,4,3,1};
+        System.out.println(buyandsellstocks(prices2));
 
     }
 }
