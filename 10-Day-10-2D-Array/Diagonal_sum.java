68326115378d13af7b65904d419800aa2a79dc81
patelprince0555 <patelprince0555@gmail.com>
2024-02-24 00:28:52 +0530

    24-02-2024

--- 10-Day-10-2D-Array/Diagonal_sum.java
+++ 10-Day-10-2D-Array/Diagonal_sum.java
@@ -14,11 +14,23 @@ public class Diagonal_sum {
         return sum;
 
     }
+
+    public static int diagonslsummethod2(int matrix[][]){
+        int sum=0;
+        for(int i=0;i<matrix.length;i++){
+            sum=sum+matrix[i][i];
+            if(i!=matrix.length-1-i){
+                sum=sum+matrix[i][matrix.length-i-1];
+            }
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         int matrix[][]={{1,2,3,4},
                         {5,6,7,8},
                         {9,10,11,12},
                         {13,14,15,16}};
                         System.out.println(diagonslsum(matrix));
+                        System.out.println(diagonslsummethod2(matrix));
     }
 }
