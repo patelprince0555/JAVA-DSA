09dc1c4912d79c4d852b0b971746ff7502f53014
patelprince0555 <patelprince0555@gmail.com>
2024-02-25 13:50:23 +0530

    25-02-2025

---
+++ 11-Day-11-String/Strings.java
@@ -0,0 +1,8 @@
+public class Strings{
+    public static void main(String[] args) {
+        char arr[]={'a','b','c','d'};
+        String str ="abcd";
+        String str2=new String("prince");
+    }
+
+}
