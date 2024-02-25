0745bde416598714c7b2da69390e8ea019ddb805
patelprince0555 <patelprince0555@gmail.com>
2024-02-25 14:10:45 +0530

    25-02-2024

--- 11-Day-11-String/Strings.java
+++ 11-Day-11-String/Strings.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Strings{
     public static void main(String[] args) {
         char arr[]={'a','b','c','d'};
         String str ="abcd";
         String str2=new String("prince");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" enter your name");
+        String name = sc.nextLine();
+        System.out.println(name);
+    
     }
 
 }
