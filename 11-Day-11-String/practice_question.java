51dc0cb336c6966aa90f7b0d47735bef3ae74bb5
patelprince0555 <patelprince0555@gmail.com>
2024-02-25 20:43:26 +0530

    25-02-2024

---
+++ 11-Day-11-String/practice_question.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class practice_question {
+
+    public static void lowercasevovels(String str){
+        int count =0;
+        for(int i =0;i<str.length();i++){
+            char ch =str.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
+    public static void main(String[] args) {
+        //  question 1
+        // count how many times lowercase vowels occured in a string entered by the user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the string");
+        String sb=sc.nextLine();
+        lowercasevovels(sb);
+
+    }
+}
