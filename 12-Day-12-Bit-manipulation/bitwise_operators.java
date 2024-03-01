698af2ac0057f1ea9047b35620d0fae887656c8e
patelprince0555 <patelprince0555@gmail.com>
2024-03-01 17:06:03 +0530

    01-03-2024

--- 12-Day-12-Bit-manipulation/bitwise_operators.java
+++ 12-Day-12-Bit-manipulation/bitwise_operators.java
@@ -1,6 +1,13 @@
 public class bitwise_operators{
     public static void main(String[] args) {
-        // Binary operators
+        // Binary operators (AND)
         System.out.println(5&6);
+
+        // Binary operators (OR)
+        System.out.println(5|6);
+
+
+        // Binary operators (XOR)
+        System.out.println(5^6);
     }
 }
