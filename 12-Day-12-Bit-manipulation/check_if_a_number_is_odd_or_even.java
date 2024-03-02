19f3cf700bfec0f5ab0e98306cea6ad51fbe7996
patelprince0555 <patelprince0555@gmail.com>
2024-03-02 16:18:00 +0530

    02-03-2024

--- 12-Day-12-Bit-manipulation/check_if_a_number_is_odd_or_even.java
+++ 12-Day-12-Bit-manipulation/check_if_a_number_is_odd_or_even.java
@@ -1,5 +1,14 @@
 public class check_if_a_number_is_odd_or_even {
+    public static void evenorodd(int n){
+        int bitmask=1;
+        if((n&bitmask)==0){
+            System.out.println("it is even number");
+        }else{
+            System.out.println("it is a odd number");
+        }
+    }
     public static void main(String[] args) {
+        evenorodd(6);
         
     }
 }
