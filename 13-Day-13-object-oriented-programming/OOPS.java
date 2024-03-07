d98fcc7e044a4eee0b8b7ee815ad44da5c1467a1
patelprince0555 <patelprince0555@gmail.com>
2024-03-07 22:27:13 +0530

    07-03-2024

--- 13-Day-13-object-oriented-programming/OOPS.java
+++ 13-Day-13-object-oriented-programming/OOPS.java
@@ -1,3 +1,17 @@
 public class OOPS{
-    
+    public static void main(String[] args) {
+        pen p1 = new pen();
+    }
+
+}
+class pen{
+    String color;
+    int tip;
+
+    void setcolor(String newcolor){
+        color=newcolor;
+    }
+    void settip(int newtip){
+        tip=newtip;
+    }
 }
