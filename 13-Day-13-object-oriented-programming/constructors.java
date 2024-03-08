b4bdb165e965c8e97482626ab40222f7db71cf24
patelprince0555 <patelprince0555@gmail.com>
2024-03-08 23:01:09 +0530

    08-03-2024

--- 13-Day-13-object-oriented-programming/constructors.java
+++ 13-Day-13-object-oriented-programming/constructors.java
@@ -1,5 +1,20 @@
 public class constructors {
     public static void main(String[] args) {
+        student s1=new student();
         
+    }
+}
+class student {
+    String name;
+    int roll;
+
+    student(){
+        System.out.println("constructor is called");
+    }
+    student(String name){
+        this.name=name;
+    }
+    Student(int roll){
+        this.roll=roll;
     }
 }
