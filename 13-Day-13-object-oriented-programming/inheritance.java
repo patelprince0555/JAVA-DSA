e26ea1151eccd84f6c4c8f6c4d0021f828b0bd75
patelprince0555 <patelprince0555@gmail.com>
2024-03-09 17:36:57 +0530

    09-03-2024

---
+++ 13-Day-13-object-oriented-programming/inheritance.java
@@ -0,0 +1,31 @@
+public class inheritance {
+    public static void main(String[] args) {
+        Fish shark=new Fish();
+        shark.eat();
+        shark.color="red";
+        System.out.println(shark.color);
+        
+    }
+}
+//Base class
+class Animal{
+    String color;
+
+    void eat(){
+        System.out.println("eats");
+    }
+
+    void breathe(){
+        System.out.println("breathes");
+    }
+}
+
+
+// Derived class
+class Fish extends Animal{
+    int fins;
+
+    void swim(){
+        System.out.println("Swims in water");
+    }
+}
