dfab8ae26f628bbfcdf485ab10e3bf191c1ae50c
patelprince0555 <patelprince0555@gmail.com>
2024-03-12 23:18:55 +0530

    12-03-2024

--- 14-Day-14-Recursion/print_numbers_in_decreasing_order.java
+++ 14-Day-14-Recursion/print_numbers_in_decreasing_order.java
@@ -1,5 +1,16 @@
 public class print_numbers_in_decreasing_order{
+    public static void printdec(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n+" ");
+        printdec(n-1);
+    }
     public static void main(String[] args) {
+        int n=10;
+        printdec(n);
+
         
     }
 }
