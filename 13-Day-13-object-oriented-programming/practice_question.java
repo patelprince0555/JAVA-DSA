1a92ed0e8b8a4bf7d62efb544fe1afdbcb8a91bc
patelprince0555 <patelprince0555@gmail.com>
2024-03-12 19:08:54 +0530

    12-03-2024

--- 13-Day-13-object-oriented-programming/practice_question.java
+++ 13-Day-13-object-oriented-programming/practice_question.java
@@ -1,21 +1,42 @@
 public class practice_question {
     public static void main(String[] args) {
         // what will be output of this code
-        vechicle obj1 =new car();
-        obj1.print();
+//         vechicle obj1 =new car();
+//         obj1.print();
 
-        vechicle obj2 = new vechicle();
-        obj2.print();
-    }
+//         vechicle obj2 = new vechicle();
+//         obj2.print();
+//     }
+// }
+
+// class vechicle{
+//     void print(){
+//         System.out.println("base class (vehicle)");
+//     }
+// }
+// class car extends vechicle{
+//     void print(){
+//         System.out.println("derived class (car)");
+//     }
+// }
+
+
+vechicle obj1 =new car();
+// obj1.print1(); give an error
+
+vechicle obj2 = new vechicle();
+obj2.print();
+}
 }
 
 class vechicle{
-    void print(){
-        System.out.println("base class (vehicle)");
-    }
+void print(){
+System.out.println("base class (vehicle)");
+}
 }
 class car extends vechicle{
-    void print(){
-        System.out.println("derived class (car)");
-    }
+void print1(){
+System.out.println("derived class (car)");
 }
+}
+
