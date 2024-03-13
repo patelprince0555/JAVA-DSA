6b6c2592ddb38fc0e6b150e285e3aa770477a365
patelprince0555 <patelprince0555@gmail.com>
2024-03-13 23:59:11 +0530

    13-03-2024

--- 14-Day-14-Recursion/print_numbers_in_ascending_order.java
+++ 14-Day-14-Recursion/print_numbers_in_ascending_order.java
@@ -1,5 +1,14 @@
 public class print_numbers_in_ascending_order {
+    public static void printascending(int n){
+        if(n==10){
+            System.out.println(10);
+            return;
+        }
+        System.out.print(n+" ");
+        printascending(n+1);
+    }
     public static void main(String[] args) {
+        printascending(1);
         
     }
 }
