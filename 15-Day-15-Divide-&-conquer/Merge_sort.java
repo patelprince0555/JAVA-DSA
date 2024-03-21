dc30f2714e759e569673fe0c160e456af6d7aff7
patelprince0555 <patelprince0555@gmail.com>
2024-03-21 16:28:02 +0530

    21-03-2024

---
+++ 15-Day-15-Divide-&-conquer/Merge_sort.java
@@ -0,0 +1,11 @@
+public class Merge_sort{
+    public static void printarr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        
+    }
+}
