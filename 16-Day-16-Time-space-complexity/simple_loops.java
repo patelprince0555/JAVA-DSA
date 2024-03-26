4609ede2a6cfad0f99e03c27bfbe47a0b6fb1a5c
patelprince0555 <patelprince0555@gmail.com>
2024-03-26 18:14:11 +0530

    26-03-2024

---
+++ 16-Day-16-Time-space-complexity/simple_loops.java
@@ -0,0 +1,8 @@
+public class simple_loops {
+    public static void main(String[] args) {
+        int n =20;
+        for(int i=0;i<n;i++){
+        }
+        // so for simple for loop time complexity is o(n)
+    }
+}
