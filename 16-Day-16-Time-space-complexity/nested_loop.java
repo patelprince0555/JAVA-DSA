1f677def49e4a0d992c03c02210a110dc5d19454
patelprince0555 <patelprince0555@gmail.com>
2024-03-26 18:26:46 +0530

    26-03-2024

---
+++ 16-Day-16-Time-space-complexity/nested_loop.java
@@ -0,0 +1,10 @@
+public class nested_loop {
+    public static void main(String[] args) {
+        // nested for loop
+        int n= 3;
+        for(int i=0;i<n;i++){
+            for(int j= i+1;j<n;j++);
+            //  so for the nested loop time complexity is o(n^2)
+        }
+    }
+}
