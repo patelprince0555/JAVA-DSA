517a6cf4c3974fb5f87cc99d83bbd5d611952498
patelprince0555 <patelprince0555@gmail.com>
2024-03-28 23:09:14 +0530

    28-03-2024

---
+++ 16-Day-16-Time-space-complexity/Recursion_factorial.java
@@ -0,0 +1,11 @@
+public class Recursion_factorial {
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
