5d7153df993a17e8a62404b3f2c3cb56295d8aa3
patelprince0555 <patelprince0555@gmail.com>
2024-04-01 19:19:41 +0530

    01-04-2024

--- 17-Day-17-Backtracking/practice_question.java
+++ 17-Day-17-Backtracking/practice_question.java
@@ -1,4 +1,39 @@
 public class practice_question {
+    public static boolean issafe(int maze[][],int x,int y){
+        return (x>=0 &&  x<maze.length && 
+                 y>=0 && y<maze.length && maze[x][y]==1);
+    }
+
+    public static boolean solvemaze(int maze[][]){
+        int n=maze.length;
+        int sol[][]= new int[n][n];
+        if(solvemazeutil(maze,0,0,sol)==false){
+            System.out.println("soution doesn,t exist");
+            return false;
+        }
+        printmaze(sol);
+        return true;
+    }
+
+    public static boolean solvemazeutil(int maze[][],int x,int y,int sol[][]){
+        if(x==maze.length-1&&y==maze.length-1&& maze[x][y]==1){
+            sol[x][y]=1;
+            return true;
+        }
+        // check if maze [x][y] is valid
+        if(issafe(maze, x, y)==true){
+            if(sol[x][y]==1)
+            return false;
+            sol[x][y]=1;
+            if(solvemazeutil(maze, x+1, y, sol))
+            return true;
+            if(solvemazeutil(maze, x, y+1, sol))
+            return true;
+            sol[x][y]=0;
+            return false;
+        }
+        return false;
+    }
     public static void printmaze(int maze[][]){
         for(int i=0;i<maze.length;i++){
             for(int j=0;j<maze.length;j++){
@@ -14,6 +49,6 @@ public class practice_question {
                      {1,1,0,1},
                      {0,1,0,0},
                      {1,1,1,1}};
-                     printmaze(maze);
+                    solvemaze(maze);
     }
 }
