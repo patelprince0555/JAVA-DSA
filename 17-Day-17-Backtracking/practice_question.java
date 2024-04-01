09ace3ae03c69d120c1713ff3c5e4cef3fcce790
patelprince0555 <patelprince0555@gmail.com>
2024-04-01 18:50:57 +0530

    01-04-2024

--- 17-Day-17-Backtracking/practice_question.java
+++ 17-Day-17-Backtracking/practice_question.java
@@ -1,4 +1,12 @@
 public class practice_question {
+    public static void printmaze(int maze[][]){
+        for(int i=0;i<maze.length;i++){
+            for(int j=0;j<maze.length;j++){
+                System.out.print(maze[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         //  question 1 
         //  rat in a maze
@@ -6,5 +14,6 @@ public class practice_question {
                      {1,1,0,1},
                      {0,1,0,0},
                      {1,1,1,1}};
+                     printmaze(maze);
     }
 }
