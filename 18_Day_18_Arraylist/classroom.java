f5c3487e2b287b7eeffe494e801e250fcc53afa9
patelprince0555 <patelprince0555@gmail.com>
2024-04-12 15:15:01 +0530

    12-04-2024

--- 18_Day_18_Arraylist/classroom.java
+++ 18_Day_18_Arraylist/classroom.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class classroom{
     public static void main(String[] args) {
-        
+        ArrayList<Integer> list=new ArrayList<>();
+        ArrayList<String> list2=new ArrayList<>();
+        ArrayList<Boolean> list3= new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
     }
 }
