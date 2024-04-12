e58e1efb76226410047083ae285e7e3cc135cd77
patelprince0555 <patelprince0555@gmail.com>
2024-04-12 15:32:37 +0530

    12-04-2024

--- 18_Day_18_Arraylist/classroom.java
+++ 18_Day_18_Arraylist/classroom.java
@@ -4,18 +4,30 @@ public class classroom{
         ArrayList<Integer> list=new ArrayList<>();
         ArrayList<String> list2=new ArrayList<>();
         ArrayList<Boolean> list3= new ArrayList<>();
-
+// add function
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
         System.out.println(list);
 
+//  get function
         int element =list.get(1);
         System.out.println(element);
 
+//  remove function
         list.remove(3);
         System.out.println(list);
-        
+
+ // set element
+        list.set(0, 0);
+        System.out.println(list);
+// contain element
+
+System.out.println(list.contains(12));
+System.out.println(list.contains(0));
+
+ 
+
     }
 }
