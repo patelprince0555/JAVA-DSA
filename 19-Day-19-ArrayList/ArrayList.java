e23e07d677f4e36b581c567a6552139a6cc597b8
patelprince0555 <patelprince0555@gmail.com>
2024-04-12 15:00:29 +0530

    12-04-2024

--- 19-Day-19-ArrayList/ArrayList.java
+++ 19-Day-19-ArrayList/ArrayList.java
@@ -1,11 +1,18 @@
-import java.util.*;
+import java.util.ArrayList;
 public class Arraylist{
     public static void main(String[] args) {
         // java collection framework
         // className objectName=new className();
         ArrayList<Integer> list=new ArrayList<>();
-        ArrayList<String> list2=new ArrayList<>();
-        ArrayList<Boolean> list4=new ArrayList<>();
+        // ArrayList<String> list2=new ArrayList<>();
+        // ArrayList<Boolean> list4=new ArrayList<>();
+
+
+       list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
 
     }
 }
