e07407eff083a94b7100d093ee21e499dd981e2c
patelprince0555 <patelprince0555@gmail.com>
2024-04-14 17:42:40 +0530

    14-04-2024

--- 18_Day_18_Arraylist/Container_with_most_water.java
+++ 18_Day_18_Arraylist/Container_with_most_water.java
@@ -1,17 +1,39 @@
 import java.util.*;
 public class Container_with_most_water {
-    public static int storewater(ArrayList<Integer> height){
-        int maxwater=0;
-        for(int i=0;i<height.size();i++){
-            for(int j=i+1;j<height.size();j++){
-                int ht =Math.min(height.get(i), height.get(j));
-                int wt=j-i;
-                int currrwater =ht*wt;
-                maxwater=Math.max(maxwater, currrwater);
-            }
+//     public static int storewater(ArrayList<Integer> height){
+//         int maxwater=0;
+//         for(int i=0;i<height.size();i++){
+//             for(int j=i+1;j<height.size();j++){
+//                 int ht =Math.min(height.get(i), height.get(j));
+//                 int wt=j-i;
+//                 int currrwater =ht*wt;
+//                 maxwater=Math.max(maxwater, currrwater);
+//             }
+//         }
+//         return maxwater;
+//     }
+
+// 2 pointer apporach
+public static int storewater(ArrayList<Integer> height){
+    int maxwater= 0;
+    int lp=0;
+    int rp=height.size()-1;
+    while(lp<rp){
+        // calaculate water area
+        int ht=Math.min(height.get(lp),height.get(rp));
+        int width =rp-lp;
+        int currwater =ht*width;
+        maxwater=Math.max(maxwater,currwater);
+
+        // update ptr
+        if(height.get(lp)<height.get(rp)){
+            lp++;
+        }else{
+            rp--;
         }
-        return maxwater;
     }
+    return maxwater;
+}
     public static void main(String[] args) {
         ArrayList<Integer> height=new ArrayList<>();
         height.add(1);
