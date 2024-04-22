0c2c7d1e1f950fef83df9f75de2613d79424a349
patelprince0555 <patelprince0555@gmail.com>
2024-04-22 21:06:17 +0530

    22-04-2024

--- 19-Day-19-Linked list(part-1)/Search_iterative.java
+++ 19-Day-19-Linked list(part-1)/Search_iterative.java
@@ -8,7 +8,62 @@ public class Search_iterative {
         }
     }
 
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addfirst(int data){
+        // step 1= create new node
+        Node newNode = new Node(data);
+        size++;
+        if(head==null){
+            head= tail=newNode;
+            return ;
+
+        }
+        // step 2 newnode next= head
+        newNode.next=head;
+
+        // step 3 head = newnode
+        head= newNode;
+    }
+
+    public void addlast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head==null){
+            head= tail=newNode;
+            return ;
+
+        }
+        tail.next=newNode;
+        tail=newNode;
+    }
+
+    public void print(){
+        if(head  ==null){
+            System.out.println("ll is empty");
+            return;
+        }
+        Node temp =head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+
+
     public static void main(String[] args) {
+
+        Search_iterative ll =new Search_iterative();
+        ll.addfirst(1);
+        ll.addlast(2);
+        ll.addlast(3);
+        ll.addlast(4);
+        ll.addlast(5);
+        ll.print();
+
         
     }
 }
