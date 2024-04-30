097fccc5f4a356073f9add28c49c6dc518a7f988
patelprince0555 <patelprince0555@gmail.com>
2024-04-30 11:58:44 +0530

    30-04-2024

--- 20-Day-20-Linked list(part-2)/index.java
+++ 20-Day-20-Linked list(part-2)/index.java
@@ -1,4 +1,19 @@
 public class index{
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+
+    
     public static void main(String[] args) {
         
     }
