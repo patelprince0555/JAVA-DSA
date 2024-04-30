350ef6166df9e82a1da78912b8ff5c04615312de
patelprince0555 <patelprince0555@gmail.com>
2024-04-30 12:09:26 +0530

    30-04-2024

--- 20-Day-20-Linked list(part-2)/index.java
+++ 20-Day-20-Linked list(part-2)/index.java
@@ -55,8 +55,28 @@ public class index{
         System.out.println("null");
     }
 
-    
+    public static boolean iscycyle(){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;//+1
+            fast =fast.next.next;//+2
+
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;// cylcle doesn't exist
+
+    }
     public static void main(String[] args) {
+        head= new Node(1);
+        head.next = new Node(2);
+        head.next.next=new Node(3);
+        head.next.next.next= head;
+        // 1->2->3->1
+        System.out.println(iscycyle());
         
     }
 }
