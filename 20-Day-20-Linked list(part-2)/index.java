2a7240df22bf8e3ec6c70594c6c97632d0bcaa1f
patelprince0555 <patelprince0555@gmail.com>
2024-04-30 14:57:18 +0530

    30-04-2024

--- 20-Day-20-Linked list(part-2)/index.java
+++ 20-Day-20-Linked list(part-2)/index.java
@@ -70,13 +70,46 @@ public class index{
         return false;// cylcle doesn't exist
 
     }
+    public static  void removecycle(){
+        //detect cycle
+        Node slow = head;
+        Node fast= head;
+        boolean cycle =false;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast= fast.next.next;
+            if(fast==slow){
+                cycle=true;
+                break;
+            }
+        }
+        if(cycle==false){
+            return;
+        }
+        // find meeting point
+        slow =head;
+        Node prev =null;//last node
+        while(slow!=fast){
+            prev=fast;
+            slow=slow.next;
+            fast=fast.next;
+        }
+        // remove cycle-> last.next==null
+
+        prev.next=null;
+    }
     public static void main(String[] args) {
+        
         head= new Node(1);
-        head.next = new Node(2);
+        Node temp =new Node (2);
+        head.next=temp;
         head.next.next=new Node(3);
-        head.next.next.next= head;
-        // 1->2->3->1
+        head.next.next.next= temp;
+        // 1->2->3->2
         System.out.println(iscycyle());
+        removecycle();
+        System.out.println(iscycyle());
+         
         
     }
 }
