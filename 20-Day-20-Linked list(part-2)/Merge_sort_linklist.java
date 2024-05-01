6d06ec831f4d07e1b608940401ba59f2aa52cf99
patelprince0555 <patelprince0555@gmail.com>
2024-05-01 16:17:02 +0530

    01-05-2024

---
+++ 20-Day-20-Linked list(part-2)/Merge_sort_linklist.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Merge_sort_linklist {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.addFirst(1);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        System.out.println(ll);
+        
+    }
+}
