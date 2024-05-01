2f9b9f7691c41ba8740781da82814bf9671aa01a
patelprince0555 <patelprince0555@gmail.com>
2024-05-01 15:38:57 +0530

    01-05-2024

--- 20-Day-20-Linked list(part-2)/Classroom.java
+++ 20-Day-20-Linked list(part-2)/Classroom.java
@@ -1,6 +1,17 @@
 import java.util.*;
 public class Classroom {
     public static void main(String[] args) {
+        // create -objects, int ,float, boolean ->Integer, Float, Character
+        LinkedList<Integer> ll = new LinkedList<>();
+        // add
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(0);
+        System.out.println(ll);
+        // remove
+        ll.removeFirst();
+        ll.removeLast();
+        System.out.println(ll);
         
     }
 }
