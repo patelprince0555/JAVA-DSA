90bb76ee5507f303133fb3e3db2c4f9aeb39a178
patelprince0555 <patelprince0555@gmail.com>
2024-05-15 01:50:40 +0530

    15-05-2024

--- 21-Day-21-Stacks/Duplicate_parentheses.java
+++ 21-Day-21-Stacks/Duplicate_parentheses.java
@@ -1,5 +1,34 @@
+import java.util.*;
 public class Duplicate_parentheses {
+
+    public static boolean isDuplicate(String str){
+        Stack<Character> s= new Stack<>();
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+
+            // closing 
+            if(ch==')'){
+                int count =0;
+                while(s.peek()!='('){
+                    s.pop();
+                    count++;
+                }
+                if(count<1){
+                    return true;
+                }else{
+                    s.pop();// opening pair
+                }
+            }else{
+                s.push(ch);
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
+        String str="((a+b))";
+        String str1="(a+b)";
+        System.out.println(isDuplicate(str1));
+        System.out.println(isDuplicate(str));
         
     }
 }
