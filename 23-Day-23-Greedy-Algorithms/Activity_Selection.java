4d038abec82585a10ca0120fe165dc439924d85d
patelprince0555 <patelprince0555@gmail.com>
2024-05-20 20:24:50 +0530

    20-05-2024

--- 23-Day-23-Greedy-Algorithms/Activity_Selection.java
+++ 23-Day-23-Greedy-Algorithms/Activity_Selection.java
@@ -4,17 +4,27 @@ public class Activity_Selection {
         int start[]={1,3,0,5,8,5};
         int end[]={2,4,6,7,9,9};
 
+
+        // sorting 
+        int activities[][]=new int[start.length][3]; 
+        for(int i=0;i<start.length;i++){
+            activities[i][0]=i;
+            activities[i][1]=start[i];
+            activities[i][2]=end[i];
+        }
+        Arrays.sort(activities , Comparator.comparingDouble(o->o[2]));
+
         int maxAct=0;
         ArrayList<Integer> ans= new ArrayList<>();
         maxAct=1;
         ans.add(0);
-        int lastend=end[0];
+        int lastend=activities[0][2];
         for(int i=1;i<end.length;i++){
-            if(start[i]>=lastend){
+            if(activities[i][1]>=lastend){
                // activity select
                maxAct++;
-               ans.add(i);
-               lastend=end[i]; 
+               ans.add(activities[i][0]);
+               lastend=activities[i][2]; 
             }
         }
         System.out.println("max activities = "+maxAct);
