07ff3396240002ba0e86227dea7b27f980d74a00
patelprince0555 <patelprince0555@gmail.com>
2024-05-22 18:03:07 +0530

    22-05-2024

---
+++ 23-Day-23-Greedy-Algorithms/Chocola_problem.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Chocola_problem {
+    public static void main(String[] args) {
+        // int n=4; int m=6;
+        Integer costver[]={2,1,3,1,4};
+        Integer costhor[]={4,1,2};
+
+        Arrays.sort(costver,Collections.reverseOrder());
+        Arrays.sort(costhor,Collections.reverseOrder());
+        int h=0;int v=0;
+        int hp=1;int vp=1;
+        int cost=0;
+        while(h<costhor.length&&v<costver.length){
+            if(costver[v]<=costhor[h]){
+                cost+=(costhor[h]*vp);
+                hp++;
+                h++;
+            }else{
+                cost+=(costver[v]*hp);
+                vp++;
+                v++;
+            }
+        }
+        while (h<costhor.length) {
+            cost+=(costhor[h]*vp);
+            hp++;
+            h++;
+        }
+        while (v<costver.length) {
+            cost+=(costver[v]*hp);
+                vp++;
+                v++;
+        }
+        System.out.println("minimum cost of cuts = "+cost);
+    }
+}
