a3fe2f0debc399d50574c7e7bc3bfacb5a384b5c
patelprince0555 <patelprince0555@gmail.com>
2024-08-03 15:47:26 +0530

    03-08-2024

---
+++ 23-Day-23-Greedy-Algorithms/tempCodeRunnerFile.java
@@ -0,0 +1,10 @@
+ // while (h<costhor.length) {
+        //     cost+=(costhor[h]*vp);
+        //     hp++;
+        //     h++;
+        // }
+        // while (v<costver.length) {
+        //     cost+=(costver[v]*hp);
+        //         vp++;
+        //         v++;
+        // }
